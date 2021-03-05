1f85a686f06641332a2811b4860232b9d1100471
Korhan Mutludoğan <korhanmd@gmail.com>
2021-03-05 13:57:10 +0000

    Change robot's initial pose

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+xterm  -e  " export ROBOT_INITIAL_POSE='-x 2 -y 2'; roslaunch turtlebot_gazebo turtlebot_world.launch " &
 sleep 5
 xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch" & 
 sleep 5
