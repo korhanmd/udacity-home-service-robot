a5982e10f8a34fee22664674ed579fdf6e976e55
Korhan Mutludoğan <korhanmd@gmail.com>
2021-03-08 20:41:43 +0000

    Edit add markers node to subscribe robot's status and publish marker according to status

--- add_markers/src/add_markers.cpp
+++ add_markers/src/add_markers.cpp
@@ -1,5 +1,12 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
+#include <std_msgs/Int32.h>
+
+int status;
+
+void status_callback(const std_msgs::Int32::ConstPtr& msg){
+  status = msg->data;
+}
 
 int main( int argc, char** argv )
 {
@@ -7,44 +14,45 @@ int main( int argc, char** argv )
   ros::NodeHandle n;
   ros::Rate r(1);
   ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+  ros::Subscriber odom_sub = n.subscribe("robot_status", 1, status_callback);
 
   // Set our initial shape type to be a cube
   uint32_t shape = visualization_msgs::Marker::CUBE;
 
-  while (ros::ok())
-  {
-    visualization_msgs::Marker marker;
-    // Set the frame ID and timestamp.  See the TF tutorials for information on these.
-    marker.header.frame_id = "map";
-    marker.header.stamp = ros::Time::now();
+  visualization_msgs::Marker marker;
+  // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+  marker.header.frame_id = "map";
+  marker.header.stamp = ros::Time::now();
 
-    // Set the namespace and id for this marker.  This serves to create a unique ID
-    // Any marker sent with the same namespace and id will overwrite the old one
-    marker.ns = "add_markers";
-    marker.id = 0;
+  // Set the namespace and id for this marker.  This serves to create a unique ID
+  // Any marker sent with the same namespace and id will overwrite the old one
+  marker.ns = "add_markers";
+  marker.id = 0;
 
-    // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
-    marker.type = shape;
+  // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
+  marker.type = shape;
 
-    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-    marker.pose.position.z = 0;
-    marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = 1.0;
+  // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+  marker.pose.position.z = 0;
+  marker.pose.orientation.x = 0.0;
+  marker.pose.orientation.y = 0.0;
+  marker.pose.orientation.z = 0.0;
+  marker.pose.orientation.w = 1.0;
 
-    // Set the scale of the marker -- 1x1x1 here means 1m on a side
-    marker.scale.x = 1.0;
-    marker.scale.y = 1.0;
-    marker.scale.z = 1.0;
+  // Set the scale of the marker -- 1x1x1 here means 1m on a side
+  marker.scale.x = 1.0;
+  marker.scale.y = 1.0;
+  marker.scale.z = 1.0;
 
-    // Set the color -- be sure to set alpha to something non-zero!
-    marker.color.r = 0.0f;
-    marker.color.g = 0.0f;
-    marker.color.b = 1.0f;
-    marker.color.a = 1.0;
+  // Set the color -- be sure to set alpha to something non-zero!
+  marker.color.r = 0.0f;
+  marker.color.g = 0.0f;
+  marker.color.b = 1.0f;
+  marker.color.a = 1.0;
 
-    marker.lifetime = ros::Duration();
+  marker.lifetime = ros::Duration();
+
+  while (ros::ok()) {
 
     // Publish the marker
     while (marker_pub.getNumSubscribers() < 1)
@@ -58,19 +66,21 @@ int main( int argc, char** argv )
     }
 
     float goal_points[2][2] = {{11.0, -11.0}, {13.5, 0.0}};
-    
-    for (int i = 0; i < 2; i++){
-      marker.pose.position.x = goal_points[i][0];
-      marker.pose.position.y = goal_points[i][1];
-      marker.action = visualization_msgs::Marker::ADD;
-      marker_pub.publish(marker);
-      ros::Duration(5.0).sleep();
 
+    // Check if object is not picked up by the robot
+    if (status % 2 == 0){
+      marker.pose.position.x = goal_points[status/2][0];
+      marker.pose.position.y = goal_points[status/2][1];
+      marker.action = visualization_msgs::Marker::ADD;
+    }
+    // If object is picked up by the robot
+    else {
       marker.action = visualization_msgs::Marker::DELETE;
-      marker_pub.publish(marker);
-      ros::Duration(5.0).sleep();
     }
 
-    return 0;
+    marker_pub.publish(marker);
+    ros::spinOnce();
   }
+
+  return 0;
 }
